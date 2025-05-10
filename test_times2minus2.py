f17988058b288ee0863bce4c0f846b67c2ed162b
Paul <Pmboxwell@gmail.com>
2025-05-10 10:27:21 +0100

    Automated commit: Removed file

--- test_times2minus2.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from seq import Seq
-
-class TestTimes2Minus2(unittest.TestCase):
-    def test_times2minus2(self): 
-        seq = Seq()
-        self.assertEqual(seq.times2minus2(3), 4)
-unittest.main()
