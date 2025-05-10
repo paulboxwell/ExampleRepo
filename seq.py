12e309e37d7637f245d63bd847f7a95c39537345
Paul <Pmboxwell@gmail.com>
2025-05-10 09:10:57 +0100

    Automated commit: Applied AI-driven modifications

--- seq.py
+++ seq.py
@@ -1,5 +1,6 @@
-def times2minus2(num):
-    result = num * 2 - 2
-    # added a print statement to check the output
-    print(result)
-    return result
+class Seq:
+    def times2minus2(self, num):
+        result = num * 2 - 2
+        # added a print statement to check the output
+        print(result)
+        return result
